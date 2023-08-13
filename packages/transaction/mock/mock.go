352eddf7877e2312f268ac2b85aa619b95e2b4d2
Abdulrahman <abudrm2020@gmail.com>
2023-08-13 13:29:57 +0300

    move tests

--- packages/transaction/mock/mock.go
+++ packages/transaction/mock/mock.go
@@ -58,3 +58,24 @@ func (m *MockTransactionData) GetCustomerById(ctx context.Context, id int32) (*s
 		Name: "Customer 1",
 	}, nil
 }
+
+func (m *MockTransactionData) GetTotalSales(ctx context.Context) (*int32, error) {
+	var a int32 = 999
+	return &a, nil
+}
+
+func (m *MockTransactionData) GetSalesByProduct(ctx context.Context, product_id int32) (*int32, error) {
+	var a int32 = 998
+	return &a, nil
+}
+
+func (m *MockTransactionData) GetTop5Customers(ctx context.Context) ([]store.CustomerTotalSpent, error) {
+	customers := []store.CustomerTotalSpent{
+		{Id: 1, Name: "Abdulrahman", TotalSpent: 5050},
+		{Id: 2, Name: "Mohammed", TotalSpent: 3030},
+		{Id: 3, Name: "Ibramim", TotalSpent: 75075},
+		{Id: 4, Name: "Omar", TotalSpent: 10010},
+		{Id: 5, Name: "Abdulaziz", TotalSpent: 2525},
+	}
+	return customers, nil
+}
